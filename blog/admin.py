c69e78b765e956d075b0d5aebc80f8845b87ebea
MohamadrezaPiri <piry.mreza@gmail.com>
2023-04-01 14:58:44 +0430

    Set "list_per_page" to 10

--- blog/admin.py
+++ blog/admin.py
@@ -9,3 +9,4 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'user', 'created_at','post_content']
     autocomplete_fields = ['user']
     search_fields = ['title']
+    list_per_page=10
