deee5ba997d26b25d3a5f403629a221b6e311b39
MohamadrezaPiri <piry.mreza@gmail.com>
2023-04-01 14:51:58 +0430

    Add "post_content" to list_display

--- blog/admin.py
+++ blog/admin.py
@@ -6,6 +6,6 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'user', 'created_at']
+    list_display = ['title', 'user', 'created_at','post_content']
     autocomplete_fields = ['user']
     search_fields = ['title']
