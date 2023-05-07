c9f15b7f69c0ab42468c6d03373fa072db5c78cb
MohamadrezaPiri <piry.mreza@gmail.com>
2023-05-07 15:59:05 +0430

    Create PostsCountFilter class in filters.py

--- blog/filters.py
+++ blog/filters.py
@@ -18,3 +18,21 @@ class CommentsCountFilter(admin.SimpleListFilter):
             return annotated_value.filter(comments_count=0)
         elif self.value() == '0<':
             return annotated_value.filter(comments_count__gt=0)
+        
+class PostsCountFilter(admin.SimpleListFilter):
+    title = 'posts count'
+    parameter_name = 'post'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('=0', 'Without post'),
+            ('0<', 'With post')
+
+        ]
+
+    def queryset(self, request, queryset):
+        annotated_value=queryset.annotate(posts_count=Count('post'))
+        if self.value() == '=0':
+            return annotated_value.filter(posts_count=0)
+        elif self.value() == '0<':
+            return annotated_value.filter(posts_count__gt=0)        
