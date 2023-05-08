c3a9f65b07b6368e7c08cf0a376c36bb53b7d888
MohamadrezaPiri <piry.mreza@gmail.com>
2023-05-08 14:11:44 +0430

    Add actions to UserAdmin class

--- blog/admin.py
+++ blog/admin.py
@@ -80,6 +80,7 @@ class UserAdmin(admin.ModelAdmin):
     list_per_page=10
     fields=['username','first_name','last_name','email','password','is_staff']
     search_fields=['username']
+    actions = ['clear_posts']
 
     @admin.display(ordering='posts')
     def posts(self, user):
